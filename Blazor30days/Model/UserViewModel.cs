4602a1ce6466d8b36e914582069c816e4548e590
Mio <mio093611@gmail.com>
2020-09-24 00:36:28 +0800

    day9 sample

---
+++ Blazor30days/Model/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor30days.Model
+{
+    public class UserViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age:　{this.Age.ToString()}";
+        }
+    }
+}
---
+++ Blazor30days/Pages/User.razor.cs
@@ -0,0 +1,19 @@
+﻿using Blazor30days.Model;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+
+namespace Blazor30days.Pages
+{
+    public partial class User
+    {
+        [Inject]
+        private ILogger<User> Logger { get; set; }
+
+        private UserViewModel user = new UserViewModel();
+
+        private void HandleValidSubmit()
+        {
+            Logger.LogInformation($"Success: {user.ToString()}");
+        }
+    }
+}
