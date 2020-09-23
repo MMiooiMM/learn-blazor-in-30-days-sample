dbc5c01f907283391cac0514714d49e1f149b995
Mio <mio093611@gmail.com>
2020-09-23 23:53:41 +0800

    init from day8

---
+++ Blazor30days/Pages/Counter.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+
+namespace Blazor30days.Pages
+{
+    public partial class Counter
+    {
+        [Inject]
+        private ILogger<Counter> Logger { get; set; }
+
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+            Logger.LogInformation(currentCount.ToString());
+        }
+    }
+}
