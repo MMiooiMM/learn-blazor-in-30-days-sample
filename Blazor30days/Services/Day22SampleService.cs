1e8af7316baba73a163d33ff5523d217ebb45c3c
Mio <mio093611@gmail.com>
2020-10-07 23:40:35 +0800

    day22 Sample

---
+++ Blazor30days/Services/Day22SampleService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.JSInterop;
+
+namespace Blazor30days.Services
+{
+    public class Day22SampleService
+    {
+        [JSInvokable]
+        public int Sum(int a, int b) => a + b;
+    }
+}
--- Blazor30days/wwwroot/js/exampleJsInterop.js
+++ Blazor30days/wwwroot/js/exampleJsInterop.js
@@ -1,5 +1,24 @@
 ﻿window.exampleJsFunctions = {
     focusElement: function (element) {
         element.focus();
+    },
+    testPromise: function () {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve('foo');
+            }, 300);
+        });
+    },
+    callDotNetAndPrintData: function (a, b) {
+        DotNet.invokeMethodAsync('Blazor30days', 'Sum', 1, 2)
+            .then(data => {
+                console.log(data);
+            });
+    },
+    callDotNetAndPrintData2: function (dotnetHelper) {
+        return dotnetHelper.invokeMethodAsync('Sum', 1, 3)
+            .then(data => {
+                console.log(data);
+            });
     }
-}
+};
